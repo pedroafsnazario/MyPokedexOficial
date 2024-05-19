537524440ec931c1406d14e4dc48cb9fc01dd4d6
pedroafsnazario <pedro905fernades@gmail.com>
2024-05-19 03:03:09 -0300

    features: Adição de API, Glide e Retrofit além de novas classes.
    ps: codigo possui erro (por enquanto).

--- app/src/main/java/com/example/mypokedexoficial/domain/Pokemon.kt
+++ app/src/main/java/com/example/mypokedexoficial/domain/Pokemon.kt
@@ -1,11 +1,13 @@
 package com.example.mypokedexoficial.domain
 
 data class Pokemon(
-    val imageUrl: String,
     val number: Int,
     val name: String,
     val types: List<PokemonType>
 ) {
+
     val formattedNumber = number.toString().padStart(3, '0')
+
+    val imageUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/$formattedNumber.png"
 }
 
