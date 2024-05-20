e9fd4a6de936b31a3c1ded7b652c553349b595fc
Brunofell <brunofelimarti@gmail.com>
2024-05-20 10:39:26 -0300

    login e cadastro /bruno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -40,7 +44,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1");
     implementation("androidx.appcompat:appcompat:1.6.1");
     implementation("com.google.android.material:material:1.11.0");
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4");
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.0");
     testImplementation("junit:junit:4.13.2");
     androidTestImplementation("androidx.test.ext:junit:1.1.5");
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1");
@@ -51,4 +56,29 @@ dependencies {
     //Retrofit e GSON
     implementation ("com.squareup.retrofit2:retrofit:2.9.0");
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0");
+
+    // login
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
 }
--- app/src/main/java/com/example/mypokedexoficial/viewmodel/PokemonViewModelFactory.kt
+++ app/src/main/java/com/example/mypokedexoficial/viewmodel/PokemonViewModelFactory.kt
@@ -5,7 +5,13 @@ import androidx.lifecycle.ViewModelProvider
 
 @Suppress("UNCHECKED_CAST")
 class PokemonViewModelFactory : ViewModelProvider.Factory {
-     fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return PokemonViewModel() as T
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PokemonViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PokemonViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
+
+    // Remova a outra função create que causa o conflito de assinatura
 }
